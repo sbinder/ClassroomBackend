4685fd09e2fcdf46e33dde4e827dbb56f77637c4
sbinder <sbinder@stdesign.com>
2017-08-26 13:57:10 -0400

    Initial commit with backend scaffolding

---
+++ ClassroomBackend/ClassHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ClassroomBackend
+{
+    public class ClassHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
