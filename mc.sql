e239bd8af482a02d4156d9d700b7dcc531a7ff1e
sbinder <sbinder@stdesign.com>
2017-09-05 19:02:51 -0400

    testing db hook 3

---
+++ mc.sql
@@ -0,0 +1,137 @@
+-- MySQL dump 10.13  Distrib 5.7.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mc
+-- ------------------------------------------------------
+-- Server version	5.7.19-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `org`
+--
+
+DROP TABLE IF EXISTS `org`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `org` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `orgid` varchar(255) NOT NULL,
+  `orgname` varchar(255) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `hash` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `orgid_UNIQUE` (`orgid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `org`
+--
+
+LOCK TABLES `org` WRITE;
+/*!40000 ALTER TABLE `org` DISABLE KEYS */;
+/*!40000 ALTER TABLE `org` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `parent`
+--
+
+DROP TABLE IF EXISTS `parent`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `parent` (
+  `pid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `org` int(10) unsigned NOT NULL,
+  `title1` varchar(255) DEFAULT NULL,
+  `lname1` varchar(255) DEFAULT NULL,
+  `fname1` varchar(255) DEFAULT NULL,
+  `email1` varchar(255) DEFAULT NULL,
+  `title2` varchar(25) DEFAULT NULL,
+  `lname2` varchar(255) DEFAULT NULL,
+  `fname2` varchar(255) DEFAULT NULL,
+  `email2` varchar(255) DEFAULT NULL,
+  `address1` varchar(255) DEFAULT NULL,
+  `address2` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `zip` varchar(255) DEFAULT NULL,
+  `comment` text,
+  PRIMARY KEY (`pid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `parent`
+--
+
+LOCK TABLES `parent` WRITE;
+/*!40000 ALTER TABLE `parent` DISABLE KEYS */;
+INSERT INTO `parent` VALUES (1,1,NULL,'plname','pfname','a@b.c',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO `parent` VALUES (2,1,NULL,'last','first','c@d.e',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `parent` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `student` (
+  `stid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `org` int(10) unsigned NOT NULL,
+  `target` date NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `parent` int(11) unsigned NOT NULL,
+  `teacher` int(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `note` text,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `expires` date NOT NULL,
+  `male` bit(1) NOT NULL,
+  `trial` bit(1) NOT NULL,
+  PRIMARY KEY (`stid`),
+  UNIQUE KEY `stid_UNIQUE` (`stid`),
+  KEY `fk_student_parent_idx` (`parent`),
+  CONSTRAINT `fk_student_parent` FOREIGN KEY (`parent`) REFERENCES `parent` (`pid`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,1,'2018-01-01','slast','sfirst',1,NULL,NULL,NULL,NULL,NULL,'2018-07-01','','\0');
+INSERT INTO `student` VALUES (2,1,'2018-07-05','student','test',2,NULL,NULL,NULL,NULL,NULL,'2017-06-30','\0','\0');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-09-05 19:02:51
