ea2280d68e61cc0f223513252a12c30d6642bbfc
Stuart Binder <sbinder@stdesign.com>
2017-09-09 23:38:09 -0400

    added routing section to Global.asax.cs

--- ClassroomBackend/Global.asax.cs
+++ ClassroomBackend/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Security;
 using System.Web.SessionState;
 
@@ -12,7 +13,8 @@ namespace ClassroomBackend
 
         protected void Application_Start(object sender, EventArgs e)
         {
-
+            GlobalConfiguration.Configuration.Routes.MapHttpRoute("Default", 
+                "{controller}/{id}", new { id = RouteParameter.Optional });
         }
 
         protected void Session_Start(object sender, EventArgs e)
