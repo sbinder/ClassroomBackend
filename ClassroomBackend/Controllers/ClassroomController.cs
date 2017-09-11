609ab7bee1e9546a1cd855a8520065feac83eccd
sbinder <sbinder@stdesign.com>
2017-09-11 13:09:10 -0400

    enabled webapi with signalr

---
+++ ClassroomBackend/Controllers/ClassroomController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ClassroomBackend.Controllers
+{
+    public class ClassroomController : ApiController
+    {
+        //[HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        //public string[] Students()
+        //{
+        //    return new string[] { "first", "second" };
+        //}
+    }
+}
--- ClassroomBackend/Startup.cs
+++ ClassroomBackend/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
+using System.Web.Http;
+using Microsoft.AspNet.SignalR;
 
 [assembly: OwinStartup("StartClassroom", typeof(ClassroomBackend.Startup))]
 namespace ClassroomBackend
@@ -10,8 +12,36 @@ namespace ClassroomBackend
     {
         public void Configuration(IAppBuilder app)
         {
-            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
-            app.MapSignalR();
+            //// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            //app.MapSignalR();
+
+            var config = new HttpConfiguration();
+
+            //I use attribute-based routing for ApiControllers
+            config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(
+                new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.Map("/signalr", map =>
+            {
+                var hubConfiguration = new HubConfiguration
+                {
+
+                };
+
+                map.RunSignalR(hubConfiguration);
+            });
+
+            config.EnsureInitialized(); //Nice to check for issues before first request
+
+            app.UseWebApi(config);
+
         }
     }
 }
