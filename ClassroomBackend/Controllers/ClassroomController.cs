12e94a6fd6995516ce8e84674d7d42bbfc308d6a
Stuart Binder <sbinder@stdesign.com>
2017-09-11 22:24:06 -0400

    moved student class into models

--- ClassroomBackend/Controllers/ClassroomController.cs
+++ ClassroomBackend/Controllers/ClassroomController.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using ClassroomBackend.Models;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -21,47 +22,18 @@ namespace ClassroomBackend.Controllers
             MySqlConnection db = new MySqlConnection(connectionString);
             MySqlCommand cmd = db.CreateCommand();
             cmd.CommandText = "select stid, target, lname, fname, liturgy from student where org = 1";
-            var students = new List<Student>();
+            List<Student> students;
             try
             {
                 db.Open();
                 MySqlDataReader reader = cmd.ExecuteReader();
-                //var columns = new List<string>();
-                //for (int i = 0; i < reader.FieldCount; i++)
-                //{
-                //    columns.Add(reader.GetName(i));
-                //}
-                while (reader.Read())
-                {
-                    var s = new Student();
-                    s.stid = reader.GetUInt32("stid");
-                    s.fname = reader.GetString("fname");
-                    s.lname = reader.GetString("lname");
-                    s.target = reader.GetDateTime("target");
-                    s.group = reader.GetInt16("liturgy");
-
-                    students.Add(s);
-                    //students.Add(reader.ToString());
-                }
+                students = Student.GetStudentList(reader);
+                return students;
             } catch (Exception r)
             {
                 Console.WriteLine("Error: " + r);                
             }
-            //return new string[] { "value1", "value2" };
-            return students;
+            return new List<Student>();
         }
-        //public string[] Students()
-        //{
-        //    return new string[] { "first", "second" };
-        //}
-       
-    }
-    public class Student
-    {
-        public uint stid;
-        public string fname;
-        public string lname;
-        public int group;
-        public DateTime target;
     }
 }
---
+++ ClassroomBackend/Models/Student.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClassroomBackend.Models
+{
+    public class Student
+    {
+        public uint stid;
+        public string fname;
+        public string lname;
+        public int group;
+        public DateTime target;
+
+        public static List<Student> GetStudentList(MySqlDataReader reader)
+        {
+            var students = new List<Student>();
+            while (reader.Read())
+            {
+                var s = new Student
+                {
+                    stid = reader.GetUInt32("stid"),
+                    fname = reader.GetString("fname"),
+                    lname = reader.GetString("lname"),
+                    target = reader.GetDateTime("target"),
+                    group = reader.GetInt16("liturgy")
+                };
+
+                students.Add(s);
+            }
+            return students;
+        }
+    }
+}
